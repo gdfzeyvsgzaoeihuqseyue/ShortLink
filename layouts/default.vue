f49d2354ad4e5c6fd16cd428351ac2310286d0b9
SteveAsterAfovo <steveasterafovo@gmail.com>
2025-08-23 05:26:23 +0100

    review default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,6 +9,6 @@
 </template>
 
 <script setup>
-import AppHeader from '~/components/App/AppHeader.vue'
-import AppFooter from '~/components/App/AppFooter.vue'
+import AppHeader from '~/components/app/AppHeader.vue'
+import AppFooter from '~/components/app/AppFooter.vue'
 </script>
